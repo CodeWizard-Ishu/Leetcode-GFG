c48369faaac29a0107bff7af6712f5e7a841616d
Utkarsh Jaiswal <89697358+CodeWizard-Ishu@users.noreply.github.com>
2024-01-17 19:08:18 +0530

    Time: 2 ms (57.71%) | Memory: 6.1 MB (99.37%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+    int dp[20]{};
     int numTrees(int n) {
         if(n <= 1) return 1;
-        int ans = 0;
+        if(dp[n]) return dp[n];
         for(int i = 1; i <= n; i++) 
-            ans += numTrees(i-1) * numTrees(n-i);
-        return ans;
+            dp[n] += numTrees(i-1) * numTrees(n-i);
+        return dp[n];
     }
 };
