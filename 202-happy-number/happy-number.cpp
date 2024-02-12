f6d527074cea55019d89e795430ad502941f4fe5
Utkarsh Jaiswal <89697358+CodeWizard-Ishu@users.noreply.github.com>
2024-02-12 22:36:20 +0530

    Time: 5 ms (16.41%) | Memory: 7.9 MB (16.31%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -10,13 +10,24 @@ public:
         return newNumber;
     }
 
+    // bool isHappy(int n) {
+    //     int slowPointer = n;
+    //     int fastPointer = nextNumber(n);
+    //     while (fastPointer != 1 && fastPointer != slowPointer) {
+    //         slowPointer = nextNumber(slowPointer);
+    //         fastPointer = nextNumber(nextNumber(fastPointer));
+    //     }
+    //     return fastPointer == 1;
+    // }
+
+    /* Approach 2  */
+    
     bool isHappy(int n) {
-        int slowPointer = n;
-        int fastPointer = nextNumber(n);
-        while (fastPointer != 1 && fastPointer != slowPointer) {
-            slowPointer = nextNumber(slowPointer);
-            fastPointer = nextNumber(nextNumber(fastPointer));
+        unordered_set<int> set;
+        while(n!=1 && !set.count(n)){
+            set.insert(n);
+            n = nextNumber(n);
         }
-        return fastPointer == 1;
+        return n==1;
     }
 };
